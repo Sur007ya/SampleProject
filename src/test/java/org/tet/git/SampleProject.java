f062df879cb42ad98abe7f54343654548e2c08aa
Sur007ya <suryamech001@gmail.com>
2022-07-19 22:31:32 -0700

    mani code

--- src/test/java/org/tet/git/SampleProject.java
+++ src/test/java/org/tet/git/SampleProject.java
@@ -1,5 +1,10 @@
 package org.tet.git;
 
 public class SampleProject {
+	public static void main(String[] args) {
+		System.out.println("MANI");
+		System.out.println("VICKY");
+		System.out.println("AJITHKUMAR");
+	}
 
 }
