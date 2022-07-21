304f5b7fc584a3a07b160775837bfff3f9b5f8e5
Sur007ya <suryamech001@gmail.com>
2022-07-20 22:57:35 -0700

    Project Drive

--- src/test/java/org/tet/git/SampleProject.java
+++ src/test/java/org/tet/git/SampleProject.java
@@ -1,5 +1,14 @@
 package org.tet.git;
 
+import org.testng.reporters.jq.Main;
+
 public class SampleProject {
+	public static void main(String[] args) {
+		System.out.println("project 1");
+		System.out.println("project 3");
+		System.out.println("project 5");
+		System.out.println("project 6");
+	
+}
 
 }
